f394166bdda3767db9d828488228b1c8b28d9ff8
David Wilemski <david@davidwilemski.com>
2012-12-09 13:43:35 -0500

    add info about the page

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World! This page is an exercise in automated deployment <a href="https://github.com/davidwilemski/deploy_test">https://github.com/davidwilemski/deploy_test</a>.'
 
 if __name__ == '__main__':
     app.run(debug=True)
